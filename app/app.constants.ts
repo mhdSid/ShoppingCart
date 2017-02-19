faec86517a5783f77a32a95b5c5079ef24f7da98
mhdSid <mohdsidani@gmail.com>
2017-02-19 06:15:38 +0200

    remove ts

--- app/app.constants.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="./../tools/typings/angularjs/angular.d.ts" />
-
-angular.module("ShoppingCart").constant("__OPTIONS__", {
-    __CartTemplateUrl__: './development/Cart/accordionTemplate.html',
-    __dummyDataUrl__: 'http://jsonplaceholder.typicode.com'
-});
--- app/app.module.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="./../tools/typings/angularjs/angular.d.ts" />
-((): void => {
-    angular.module('ShoppingCart', ['ngRoute']);
-})();
-
-
